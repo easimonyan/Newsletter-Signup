e9d4ef742c5f653cc91a37d6a03f3b67ecb211ab
easimonyan <97413445+easimonyan@users.noreply.github.com>
2022-08-12 12:42:41 -0400

    Update add.js

--- add.js
+++ add.js
@@ -1,57 +1,71 @@
 // jshint esversion: 6 
 
+// *** Constant Require Section:
+ 
 const express = require("express");
-const bodyParser = require("body-parser");
 const request = require("request");
+const bodyParser = require("body-parser");
 const https = require("https");
-const mailchimp = require("@mailchimp/mailchimp_marketing");
-
+ 
 const app = express();
-
+ 
+// *** Body Parser ***
+app.use(bodyParser.urlencoded({extended: true}));
+ 
+// *** Static Folder ***
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.get("/", function(req, res) {
+// app.use("/public", express.static(path.join(__dirname, "public")));
+ 
+// *** Tracking HTML File ***
+app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
-
-app.post("/", function(req,res) {
-    mailchimp.setConfig({
-        apiKey: "c53f5dc8bd80b05ff510fe2b7181aaac-us18",
-        server: "us18",
-      });
-      
-const listId = "a47acc395d";
-      document.querySelector("#submit").onclick = function(){
-        const firstName = document.getElementById("fName");
-        console.log(firstName.value); 
+ 
+// *** Signup Route ***
+app.post("/", function(req, res){
+ 
+    const firstName = req.body.fName;
+	const lastName = req.body.lName;
+	const email = req.body.email;
+ 
+    // *** Construct Requesting data ***
+    const data = {
+        members: [
+            {
+              email_address: email,
+              status: 'subscribed',
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName
+              }
+            }
+          ]
     }
-        const subscribingUser = {
-        firstName: "",
-        lastName: "",
-        email: ""
-        };
-
-    async function run() {
-    const response = await mailchimp.lists.addListMember(listId, {
-    email_address: subscribingUser.email,
-    status: "subscribed",
-    merge_fields: {
-      FNAME: subscribingUser.firstName,
-      LNAME: subscribingUser.lastName
-    }
+ 
+    // *** Stringify inputed data ***
+    const jsonData = JSON.stringify(data);
+ 
+    // *** url = "https://<data center>.api.mailchimp.com/3.0/lists/{listID}";
+    const url = "https://us18.api.mailchimp.com/3.0/lists/a47acc395d";
+ 
+    const options = {
+        method: "POST",
+        auth: "text or name:c53f5dc8bd80b05ff510fe2b7181aaac-us18"
+    };
+    
+    // *** Requesting and send back our data to mailchimp ***
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
     });
-
-    console.log(
-    `Successfully added contact as an audience member. The contact's id is ${
-      response.id
-    }.`
-    );
-    }
-
-    run();
-      }
-);
+ 
+    request.write(jsonData);
+    request.end();
+    
+ 
+});
+ 
 
 
 
